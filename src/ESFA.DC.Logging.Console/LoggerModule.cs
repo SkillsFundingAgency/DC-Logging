884b55cf8fe5757b3d586d70f23615d4ab38d395
Mike Childs <mike.childs@education.gov.uk>
2018-03-12 15:05:32 +0000

    Initial cut of Branch Initial012 for ESFA.DC.Logging.Console

---
+++ src/ESFA.DC.Logging.Console/LoggerModule.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using ESFA.DC.Logging.SeriLogging;
+
+namespace ESFA.DC.Logging.Console
+{
+    public class LoggerModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            var config = new ApplicationLoggerSettings();
+
+            //config.ConnectionString = "AuditLoggingConnectionString";
+            config.LoggerOutput = ESFA.DC.Logging.Enums.LogOutputDestination.SqlServer;
+
+            builder.RegisterType<SeriLogger>().As<ILogger>()
+                .WithParameter(new TypedParameter(typeof(ApplicationLoggerSettings), config));
+
+        }
+    }
+}
---
+++ src/ESFA.DC.Logging.Console/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Diagnostics.CodeAnalysis;
+using Autofac;
+
+namespace ESFA.DC.Logging.Console
+{
+    [ExcludeFromCodeCoverage]
+    class Program
+    {
+        static ILogger logger = null;
+        static void Main(string[] args)
+        {
+
+            var builder = ConfigureBuilder();
+            var container = builder.Build();
+            var scope = container.BeginLifetimeScope();
+
+            var logger = scope.Resolve<ILogger>();
+
+            //using (var logstuff = new LogStuff(builder))
+            //{
+            //    logstuff.DoStuff();
+            //}
+
+            //    return builder;
+            //})
+
+            var config = new ApplicationLoggerSettings();
+            config.EnableInternalLogs = true;
+            //using (var logger = new SeriLogger(config))
+            //{
+                logger.StartContext("Testjob");
+
+                logger.LogDebug("some debug");
+                logger.LogInfo("some info");
+                logger.LogWarning("some warn");
+                logger.LogError("some error", new Exception("there was an exception"));
+                logger.ResetContext();
+
+                logger.StartContext("Testjob2");
+            logger.StartContext("Testjob3withkey","taskKey");
+            logger.LogWarning("some warn with task key");
+            logger.ResetContext();
+            logger.LogWarning("some warn after reset");
+
+            //}
+
+            // ESFA.DC.Logging.LoggerManager
+            //logger.Flush();
+
+            // TestStackTraceLevel1();
+            //logger.CloseAndFlush();
+            System.Console.ReadLine();
+
+        }
+
+        private static ContainerBuilder ConfigureBuilder()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule<LoggerModule>();
+
+
+            return builder;
+        }
+
+        private static void TestStackTraceLevel1()
+        {
+            try
+            {
+                TestStackTraceLevel2();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("There is an error", ex);
+            }
+            
+        }
+
+        private static void TestStackTraceLevel2()
+        {
+            TestStackTraceLevel3();
+        }
+
+
+        private static void TestStackTraceLevel3()
+        {
+            try
+            {
+                var conn = new SqlConnection();
+                conn.Open();
+            }
+            catch(Exception ex)
+            {
+                throw new ArgumentNullException("this is arg null exception",ex); 
+            }
+        }
+    }
+}
